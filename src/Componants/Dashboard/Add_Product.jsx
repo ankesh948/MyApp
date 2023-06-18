73e05a381483372d334b343d67ebb130ef5c564d
Ankesh Kumar <ankeshthakur948@gmail.com>
2023-06-19 00:18:59 +0530

    night commit

--- src/Componants/Dashboard/Add_Product.jsx
+++ src/Componants/Dashboard/Add_Product.jsx
@@ -10,8 +10,9 @@ const Add_Product = () => {
   const [Description, setDescription]= useState('')
   const [Price, setPrice]= useState('')
   const [Thumbnail, setThumbnail]= useState('')
-
   const [formData, setformData] = useState('')
+
+
 
   useEffect(() => {
     const sendData = async () => {
@@ -27,12 +28,13 @@ const Add_Product = () => {
 
   function handleSubmit(event){ 
     event.preventDefault();
-    setformData({
+    const Data = {
         Title,
         Description,
         Price,
         Thumbnail
-    })
+    }
+    setformData(Data)
   }
 
 
@@ -45,26 +47,26 @@ const Add_Product = () => {
                 <div className="content-area">
                     <h1 className="title">Add Product</h1>
                     <div className="card p-4 w-100">
-                      <Form onSubmit={(event)=>handleSubmit(event)}>
+                      <Form>
                         <div className='row'>
                             <Form.Group className="col-3 mb-3">
                               <Form.Label>Title</Form.Label>
-                              <Form.Control type="text" onChange={(e)=>setTitle(e.target.value)} placeholder="Title" />
+                              <Form.Control type="text" onChange={(e)=>setTitle(e.target.value)} placeholder="Title" required />
                             </Form.Group>
                             <Form.Group className="col-3 mb-3">
                               <Form.Label>Description</Form.Label>
-                              <Form.Control type="text" onChange={(e)=>setDescription(e.target.value)} placeholder="Description" />
+                              <Form.Control type="text" onChange={(e)=>setDescription(e.target.value)} placeholder="Description" required />
                             </Form.Group>
                             <Form.Group className="col-1 mb-3">
                               <Form.Label>Price</Form.Label>
-                              <Form.Control type="text" onChange={(e)=>setPrice(e.target.value)} placeholder="Price" />
+                              <Form.Control type="text" onChange={(e)=>setPrice(e.target.value)} placeholder="Price" required />
                             </Form.Group>
                             <Form.Group className="col-3 mb-3">
                               <Form.Label>Thumbnail</Form.Label>
-                              <Form.Control type="file" onChange={(e)=>setThumbnail(e.target.value)} placeholder="Thumbnail" />
+                              <Form.Control type="file" onChange={(e)=>setThumbnail(e.target.value)} placeholder="Thumbnail" required />
                             </Form.Group>
                             <Form.Group className="col-2 ">
-                              <Form.Control type="submit" className="btn btn-info mt-4" value="Submit" />
+                              <Form.Control type="submit" className="btn btn-info mt-4" value="Submit" onSubmit={(event)=>handleSubmit(event)} />
                             </Form.Group>
                         </div>
                       
